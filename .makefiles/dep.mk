7d2af5933fd09b04b4b38135487a740c3c7fd5b1
Tyr Chen <tyr.chen@gmail.com>
2018-07-22 21:10:49 -0700

    use mix deps

---
+++ .makefiles/dep.mk
@@ -0,0 +1,10 @@
+SRC=./src
+DEPS_VER=v0.2.0
+DEPS_PREFIX=https://github.com/tyrchen/mix-deps/releases/download
+BUILDS_FILE=builds.tgz
+DEPS_FILE=deps.tgz
+BUILDS_URL=$(DEPS_PREFIX)/$(DEPS_VER)/$(BUILDS_FILE)
+DEPS_URL=$(DEPS_PREFIX)/$(DEPS_VER)/$(DEPS_FILE)
+
+extract-deps:
+	@cd $(SRC); wget $(BUILDS_URL); wget $(DEPS_URL); tar zxvf $(BUILDS_FILE); tar zxvf $(DEPS_FILE); rm $(BUILDS_FILE) $(DEPS_FILE);
