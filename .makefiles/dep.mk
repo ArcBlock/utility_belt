b13ac2fa13a10131477033f9d09333a22d107137
Tyr Chen <tyr.chen@gmail.com>
2018-07-23 08:29:49 -0700

    remove unnecessary logs

--- .makefiles/dep.mk
+++ .makefiles/dep.mk
@@ -7,4 +7,4 @@ BUILDS_URL=$(DEPS_PREFIX)/$(DEPS_VER)/$(BUILDS_FILE)
 DEPS_URL=$(DEPS_PREFIX)/$(DEPS_VER)/$(DEPS_FILE)
 
 extract-deps:
-	@cd $(SRC); wget $(BUILDS_URL); wget $(DEPS_URL); tar zxvf $(BUILDS_FILE); tar zxvf $(DEPS_FILE); rm $(BUILDS_FILE) $(DEPS_FILE);
+	@cd $(SRC); wget $(BUILDS_URL) --quiet; wget $(DEPS_URL) --quiet; tar zxf $(BUILDS_FILE); tar zxf $(DEPS_FILE); rm $(BUILDS_FILE) $(DEPS_FILE);
